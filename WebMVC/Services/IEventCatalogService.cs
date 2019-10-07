6d7ca6827ec3ea6e0fb1e8d192d41ff8434cfba7
CarleeSinton <carleen.sinton@gmail.com>
2019-10-07 08:54:36 -0700

    Connecting to github, adding service interface, and enabling searching for "all" for each category

---
+++ WebMVC/Services/IEventCatalogService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.Services
+{
+    public interface IEventCatalogService
+    {
+        Task<EventCatalog> GetEventCatalogItemsAsync(int page, int size, int? eventCategory, int? eventType, int? eventLocation);
+        Task<IEnumerable<SelectListItem>> GetEventCategoriesAsync();
+        Task<IEnumerable<SelectListItem>> GetEventTypesAsync();
+        Task<IEnumerable<SelectListItem>> GetEventLocationsAsync();
+    }
+}
