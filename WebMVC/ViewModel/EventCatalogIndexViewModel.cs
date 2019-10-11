4577337a147367983d0abbe84114cb511c487be6
Satya P Javvadi <saphin.rao@gmail.com>
2019-10-11 14:45:29 -0700

    Added ViewModel to WebMVC

---
+++ WebMVC/ViewModel/EventCatalogIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.ViewModel
+{
+    public class EventCatalogIndexViewModel
+    {
+        public PaginationInfo PaginationInfo { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public IEnumerable<SelectListItem> Types { get; set; }
+        public IEnumerable<SelectListItem> Locations { get; set; }
+        public IEnumerable<EventItem> EventItems { get; set; }
+        public int? eventCategoryFilterApplied { get; set; }
+        public int? eventTypeFilterApplied { get; set; }
+        public int? eventLocationFilterApplied { get; set; }
+
+    }
+}
