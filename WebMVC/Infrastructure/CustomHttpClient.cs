c5212d7ea0eb2b8f847f28dbae97b999a9178fcd
CarleeSinton <carleen.sinton@gmail.com>
2019-10-14 10:20:36 -0700

    Add Http Client
    - add delete, get, post, and put

---
+++ WebMVC/Infrastructure/CustomHttpClient.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.Infrastructure
+{
+    public class CustomHttpClient : IHttpClient
+    {
+        private readonly HttpClient _client;
+        public CustomHttpClient()
+        {
+            _client = new HttpClient();
+        }
+        public async Task<HttpResponseMessage> DeleteAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Delete, uri);
+            if (authorizationToken != null)
+            {
+
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue(authorizationMethod, authorizationToken);
+
+            }
+
+            return await _client.SendAsync(requestMessage);
+        }
+
+        public async Task<string> GetStringAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            var requestMessage = new HttpRequestMessage(
+                HttpMethod.Get, uri);
+
+            if (authorizationToken != null)
+            {
+                requestMessage.Headers.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue(
+                        authorizationMethod,
+                        authorizationToken);
+            }
+
+            var response = await _client.SendAsync(requestMessage);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        private async Task<HttpResponseMessage> DoPostPutAsync<T>(HttpMethod method, string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            if (method != HttpMethod.Post && method != HttpMethod.Put)
+            {
+                throw new ArgumentException("Value must be either post or put.", nameof(method));
+            }
+
+            // a new StringContent must be created for each retry 
+            // as it is disposed after each call
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, uri);
+
+            requestMessage.Content = new StringContent(JsonConvert.SerializeObject(item), System.Text.Encoding.UTF8, "application/json");
+            //  SetAuthorizationHeader(requestMessage);
+            if (authorizationToken != null)
+            {
+
+
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue(authorizationMethod, authorizationToken);
+
+            }
+
+
+            //if (requestId != null)
+            //{
+            //    requestMessage.Headers.Add("x-requestid", requestId);
+            //}
+
+            var response = await _client.SendAsync(requestMessage);
+
+            // raise exception if HttpResponseCode 500 
+            // needed for circuit breaker to track fails
+
+            if (response.StatusCode == HttpStatusCode.InternalServerError)
+            {
+                throw new HttpRequestException();
+            }
+
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> PostAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            return await DoPostPutAsync(HttpMethod.Post, uri, item, authorizationToken, authorizationMethod);
+        }
+
+        public async Task<HttpResponseMessage> PutAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            return await DoPostPutAsync(HttpMethod.Put, uri, item, authorizationToken, authorizationMethod);
+        }
+    }
+}
